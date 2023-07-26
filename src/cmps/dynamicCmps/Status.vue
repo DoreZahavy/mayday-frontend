99bcc6dc84fb54c527873d2018e42bdb7c2a3c76
Din Ben Dor <din1993@gmail.com>
2023-07-26 19:48:02 +0300

    Bug fixes in the status and priority cmps

--- src/cmps/dynamicCmps/Status.vue
+++ src/cmps/dynamicCmps/Status.vue
@@ -1,5 +1,13 @@
 <template>
-  <p v-if="status" class="status">{{ status }}</p>
+  <el-tooltip placement="bottom" trigger="click" effect="light">
+    <template #content>
+      <div v-for="status in statusOptions" :key="status" :class="status.class" class="status-option"
+        @click.stop="onSetStatus(status)">
+        {{ status.txt }}
+      </div>
+    </template>
+    <div v-if="status" class="status status-content">{{ status.txt }}</div>
+  </el-tooltip>
 </template>
 
 <script>
@@ -25,7 +33,44 @@ export default {
 
 <style lang="scss" scoped>
 .status {
-  /* background-color: lightpink; */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
   height: 100%;
+  position: relative;
+  cursor: pointer;
+  transition: 0.3s ease;
+
+  &:hover {
+    background-color: $status-done-hover;
+  }
+
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    border-right: 10px solid white;
+    border-bottom: 10px solid darken($status-done, 10%);
+    width: 0.1px;
+    height: 0.1px;
+    z-index: 2;
+    transition: transform 0.9s ease;
+  }
+
+  &:hover:before {
+    opacity: 1;
+  }
+}
+
+.status-content {
+  position: relative;
+  z-index: 1;
+}
+
+.status-option {
+  padding: 5px;
+  cursor: pointer;
 }
 </style>
