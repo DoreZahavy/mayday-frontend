bc75847825defe51431ec508cc620873e88b0dd8
Din Ben Dor <din1993@gmail.com>
2023-07-27 22:48:09 +0300

    Added file cmp vue template and styling

--- src/cmps/dynamicCmps/Attachments.vue
+++ src/cmps/dynamicCmps/Attachments.vue
@@ -1,15 +1,23 @@
 <template>
-    <article class="files">
-        <p>N</p>
+    <article class="attachments">
+        <span class="icon file-icon" v-html="getSvg('file')"></span>
+        <span class="icon plus-icon" v-html="getSvg('addUser')"></span>
     </article>
 </template>
   
 <script>
+import { svgService } from '../../services/svg.service';
+
 export default {
 
     name: "Attachments",
     props: {
         info: Array,
     },
+    methods: {
+        getSvg(iconName) {
+            return svgService.getSvg(iconName)
+        }
+    }
 }
 </script>
