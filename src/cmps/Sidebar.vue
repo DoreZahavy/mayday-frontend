93c50df8bffd651e2afedeabb60a52f7fd4933c6
Yona <dvsscheme@gmail.com>
2023-07-28 12:07:18 +0300

    style group header

--- src/cmps/Sidebar.vue
+++ src/cmps/Sidebar.vue
@@ -1,31 +1,4 @@
-<template>
-    <aside class="sidebar">
 
-        <div class="divider-div">
-            <ul class="clean-list sidebar-list nav-list">
-                <li>
-                    <div>
-                        <span v-html="getSvg('home')"></span>
-                        <RouterLink to="/">Home</RouterLink>
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <div class="sidebar-board-utils flex space-between">
-            <SidebarFilter @filter="setFilter" />
-            <div @click="onAddBoard" class="add-board-btn" v-html="getSvg('addBoard')"></div>
-        </div>
-
-        <ul class="clean-list sidebar-list">
-            <li v-for="board in boardList" 
-                :class="(board._id === boardId) ? 'selected flex align-center' : 'flex align-center'">
-                <div v-html="getSvg('boardType')" class="flex align center"></div>
-                <RouterLink class="board-link" :to="'/board/' + board._id">{{ board.title }}</RouterLink>
-                <div @click="onRemoveBoard(board._id)" v-html="getSvg('trash')" class="flex align center"></div>
-            </li>
-        </ul>
-    </aside>
-</template>
 
 <script>
 import { ref } from 'vue'
