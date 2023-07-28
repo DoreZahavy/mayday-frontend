089a7e03bf3335142dcb45315f7c63d98af92095
Yona <dvsscheme@gmail.com>
2023-07-28 15:40:16 +0300

    basic implementation of board details modal

---
+++ src/cmps/BoardInfoModal.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="board-info-background" @click="this.$emit('closeModal')">
+    </div>
+    <section class="board-info">
+        <article class="board-info-inputs">
+            title <br>
+            description
+        </article>
+        <article class="board-info-details">
+
+        </article>
+    </section>
+</template>
+<script>
+export default {
+    props:
+    {
+        board: Object
+    },
+    created(){
+    }
+}
+</script>
