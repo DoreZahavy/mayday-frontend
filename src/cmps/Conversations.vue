6ce0fb929c18e2b1635e315df7aed9e3b2df23be
Din Ben Dor <din1993@gmail.com>
2023-07-31 00:37:03 +0300

    Added drawer modal and activities + conversations cmps with custom routing

---
+++ src/cmps/Conversations.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>Conversations {{ taskId }}</h2>
+</template>
+
+<script>
+export default {
+    props: {
+        taskId: String
+    },
+    data() {
+        return {
+        }
+    },
+    created() {
+    },
+    methods: {
+    }
+}
+</script>
--- src/cmps/Sidebar.vue
+++ src/cmps/Sidebar.vue
@@ -9,9 +9,7 @@ export default {
         return {
             filterBy: '',
             active: this.boardId,
-            isCollapsed: false,
-            showModal: true
-
+            isCollapsed: false
         }
     },
     created() {
@@ -112,69 +110,8 @@ export default {
                 <!-- <p class="board-link" @click="loadBoard(board._id)">{{ board.title }}</p> -->
                 <div class="trash-board" @click="onRemoveBoard(board._id)" v-html="getSvg('trash')"></div>
             </li>
-            <div>
-                <button @click="showModal = true">Open Modal</button>
-
-                <transition name="slide">
-                    <div class="modal" v-if="showModal">
-                        <div class="modal-header">
-                            <span class="close-button" @click="showModal = false" v-html="getSvg('xButton')"></span>
-                            <h2>Social Media Campaign - #NewRelease</h2>
-                        </div>
-                    </div>
-                </transition>
-            </div>
         </ul>
         <div class="collapse-arrow" v-html="getSvg('arrowLeft')" @click="isCollapsed = !isCollapsed"></div>
         <!-- <div v-else class="collapse-arrow" v-html="getSvg('arrowRight')" @click="isCollapsed = !isCollapsed"></div> v-if="!isCollapsed" -->
     </aside>
 </template>
-
-<style scoped>
-.modal {
-    position: fixed;
-    top: 0;
-    right: 0;
-    width: 50%;
-    height: 100%;
-    z-index: 90;
-}
-
-.modal-header {
-    background-color: #fff;
-    color: #323338;
-    width: 100%;
-    height: 100%;
-    overflow: auto;
-    font-weight: 300;
-    letter-spacing: 0.8px;
-    padding-top: 70px;
-    padding-left: 28px;
-    box-shadow: -1px 0 10px rgba(0, 0, 0, 0.1);
-}
-
-.close-button {
-    position: absolute;
-    top: 25px;
-    left: 28.5px;
-    color: #777;
-    font-size: 28px;
-    cursor: pointer;
-    scale: 2;
-}
-
-.slide-enter-active,
-.slide-leave-active {
-    transition: all 0.4s ease;
-}
-
-.slide-enter-from,
-.slide-leave-to {
-    transform: translateX(100%);
-}
-
-.slide-enter-to,
-.slide-leave-from {
-    transform: translateX(0);
-}
-</style>
