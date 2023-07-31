4293f984c69b5a16c146ef417753c9a948a60e74
Din Ben Dor <din1993@gmail.com>
2023-07-31 21:18:01 +0300

    Added the number progress bar cmp functionality and styling

--- src/cmps/progressCmps/NumberProgress.vue
+++ src/cmps/progressCmps/NumberProgress.vue
@@ -1,18 +1,44 @@
 <template>
-    <section>
-        nums
-    </section>
+    <div class="number-progress-bar fs15">
+        {{ totalNumber.toLocaleString() }}
+    </div>
+    <div>sum</div>
 </template>
 
 <script>
-
 export default {
-    name: "NumberProgress",
-    props: {
-        group: Object,
-        cmpOrder: Array
-    },
+    name: 'NumberProgress',
+    props: ['group'],
+    computed: {
+        totalNumber() {
+            let total = 0
+            for (let task of this.group.tasks) {
+                if (task.Number !== null) {
+                    total += task.Number
+                }
+            }
+            return total
+        }
+    }
+}
+</script>
 
+<style scoped>
+.number-progress-bar {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 0.05em;
+    margin-left: 0.3em;
+    height: 67.93%;
+    width: 92.8%;
+    font-size: 1em;
+    color: #323338;
 }
 
-</script>
+div:nth-of-type(2) {
+    margin-left: 41%;
+    margin-top: -4%;
+    font-size: 0.85em;
+}
+</style>
