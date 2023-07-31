2d1472a80d3acaa4ee5c568ad4862409cd256f38
Dore Zahavy <dorezahavy@gmail.com>
2023-07-31 17:51:13 +0300

    import files tooltip

--- src/cmps/dynamicCmps/Members.vue
+++ src/cmps/dynamicCmps/Members.vue
@@ -1,43 +1,41 @@
 <template >
- 
-<!-- <section class="members-cmp"> -->
+  <!-- <section class="members-cmp"> -->
 
   <el-tooltip placement="bottom" trigger="click" effect="light">
     <template #content>
-        <div class="user-container">
-          <div v-if="hasMembers" class="user-pill" v-for="member in members" :key="member._id">
-            <img :src="member.imgUrl">
-            <span>{{ member.fullname }}</span>
-            <span @click="removeMember(member._id)" class="x-btn" v-icon="'xButton'"></span>
-          </div>
-        </div>
-        <input class="members-search" type="search" placeholder="Search names">
-        <ul class="user-list clean-list">
-          <li class="suggested-member" v-for="member in optionalMembers" @click="addMember(member)">
-          <img :src="member.imgUrl" >
+      <div class="user-container">
+        <div v-if="hasMembers" class="user-pill" v-for="member in info" :key="member._id">
+          <img :src="member.imgUrl">
           <span>{{ member.fullname }}</span>
-          </li>
-        </ul>
+          <span @click="removeMember(member._id)" class="x-btn" v-icon="'xButton'"></span>
+        </div>
+      </div>
+      <input class="members-search" type="search" placeholder="Search names">
+      <ul class="user-list clean-list">
+        <li class="suggested-member" v-for="member in optionalMembers" @click="addMember(member)">
+          <img :src="member.imgUrl">
+          <span>{{ member.fullname }}</span>
+        </li>
+      </ul>
+    </template>
+    <section class="members-list">
+
+      <template v-if="hasMembers && members.length > 2">
+        <MemberPreview :member="members[0]" />
+        <div v-if="members.length > 2" class="member-preview extra-persons">+{{ members.length - 1 }}</div>
       </template>
-      <section  class="members-list">
-        
-        <template v-if="hasMembers && members.length>2">
-          <MemberPreview :member="members[0]" />
-          <div v-if="members.length>2" class="member-preview extra-persons">+{{ members.length-1 }}</div>
-        </template>
-        
-      <template else v-else-if="hasMembers && members.length>0">
+
+      <template else v-else-if="hasMembers && members.length > 0">
         <MemberPreview v-for="(member, idx) in members" :key="idx" :member="member" />
       </template>
-      
+
       <span class="person-icon" v-else v-icon="'person'"></span>
       <!-- <span class="person-icon" v-else v-html="getSvg('person')"></span> -->
       <span class="plus-icon" v-icon="'plusSign'"></span>
-      
+
     </section>
   </el-tooltip>
-<!-- </section> -->
-
+  <!-- </section> -->
 </template>
   
 <script>
@@ -60,7 +58,7 @@ export default {
     hasMembers() {
       return (this.info[0]?._id)
     },
-    optionalMembers(){
+    optionalMembers() {
       let optionalMembers = this.$store.getters.boardMembers
       return optionalMembers
     }
@@ -69,16 +67,16 @@ export default {
     getSvg(iconName) {
       return svgService.getSvg(iconName)
     },
-    removeMember(memberId){
+    removeMember(memberId) {
       // const members= JSON.parse(JSON.stringify(this.info))
       // console.log('this.members:', this.members)
       // console.log(this.members);
-      const memberIdx = this.members.findIndex(m=>m._id===memberId)
-      this.members.splice(memberIdx,1)
+      const memberIdx = this.members.findIndex(m => m._id === memberId)
+      this.members.splice(memberIdx, 1)
       this.$emit('update', this.members)
 
     },
-    addMember(member){
+    addMember(member) {
       this.members.push(member)
       this.$emit('update', JSON.parse(JSON.stringify(this.members)))
 
--- src/cmps/progressCmps/AttachmentsProgress.vue
+++ src/cmps/progressCmps/AttachmentsProgress.vue
@@ -1,6 +1,30 @@
 <template>
     <article>
-   files
+        <div class="file-list flex align-center" v-if="files.length > 4">
+            <AttachmentPreview @click.stop="openModal(files[0])" :file="files[0]" class="file-preview" />
+            <AttachmentPreview @click.stop="openModal(files[1])" :file="files[1]" class="file-preview" />
+            <AttachmentPreview @click.stop="openModal(files[2])" :file="files[2]" class="file-preview" />
+            <!-- <div class="file-preview">
+
+                    <img :src="files[0]">
+                </div>
+                <div class="file-preview">
+
+                    <img :src="files[1]">
+                </div>
+                <div class="file-preview">
+
+                    <img :src="files[2]">
+                </div> -->
+            <div class="extra-files">+{{ files.length - 3 }}</div>
+        </div>
+        <div class="file-list flex align-center" v-else-if="files.length > 0">
+            <AttachmentPreview v-for="file in files" @click.stop="openModal(file)" :file="file" class="file-preview" />
+
+            <!-- <div class="file-preview" v-for="fileUrl in files">
+                    <img :src="fileUrl">
+                </div> -->
+        </div>
     </article>
 </template>
   
@@ -13,7 +37,22 @@ export default {
         group: Object,
         cmpOrder: Array
     },
-    
+    methods: {
+        openModal(file) {
+            this.$store.commit({ type: 'fileModal', file })
+        },
+    },
+    computed: {
+        files() {
+            var files = []
+            group.tasks.forEach(task => {
+                files.concat(task.files)
+            })
+            console.log('files:', files)
+            return files
+        }
+    }
+
 
 }
 </script>
