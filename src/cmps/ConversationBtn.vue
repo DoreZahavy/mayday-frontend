dd0a48110330cab46e2b0ceb9ebcba08b5bde639
Yona <dvsscheme@gmail.com>
2023-07-31 11:18:43 +0300

    added conversation button

---
+++ src/cmps/ConversationBtn.vue
@@ -0,0 +1,18 @@
+<template>
+    <button v-html="getSvg('addMessage')" class="button-as-link">
+
+    </button>
+</template>
+<script>
+import {svgService} from '@/services/svg.service';
+export default {
+    props: ["taskId"],
+    created() {
+    },
+    methods: {
+        getSvg(iconName) {
+            return svgService.getSvg(iconName)
+        }
+    }
+}
+</script>
