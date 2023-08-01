5ecfb7eea3bfbc8fb0424518a605d656c33d4bc8
Din Ben Dor <din1993@gmail.com>
2023-08-01 20:20:00 +0300

    Modified activities styling and layout

--- src/cmps/Activities.vue
+++ src/cmps/Activities.vue
@@ -3,11 +3,12 @@
         <h2>Activities</h2>
         <ul class="activity-list">
             <li class="activity-item" v-for="activity in activities" :key="activity.id">
-                <span v-html="getSvg('activityTime')"></span>
-                <span class="time-passed">{{ getTimePassed(activity.createdAt) }}</span>
+                <span><span v-html="getSvg('activityTime')"></span><span class="time-passed"> {{
+                    getTimePassed(activity.createdAt) }}</span></span>
                 <img v-if="activity.byMember" :src="activity.byMember.imgUrl" class="user-image" alt="User" />
                 <span v-else v-html="getSvg('person')"></span>
-                <span class="task-name">{{ activity.taskName }}</span>
+                <span class="task-name" :title="activity.taskName.charAt(0).toUpperCase() + activity.taskName.slice(1)">{{
+                    activity.taskName }}</span>
                 <span v-if="activity.propType.toLowerCase() === 'status' || activity.propType.toLowerCase() === 'priority'"
                     class="activity-type-icon" v-html="getSvg('statusActivity')"></span>
                 <span v-else-if="activity.propType.toLowerCase() === 'number'" class="activity-type-icon"
@@ -26,16 +27,15 @@
                 <span class="activity-type"
                     :title="activity.propType.charAt(0).toUpperCase() + activity.propType.slice(1)">{{
                         activity.propType.charAt(0).toUpperCase() + activity.propType.slice(1) }}</span>
-                <div class="changed">
-                    <span class="changed-from"
-                        :title="typeof activity.updateFrom === 'number' || typeof activity.updateFrom === 'string' ? activity.updateFrom : ''">{{
-                            activity.updateFrom }}</span>
-                    <span
-                        v-if="activity.propType.toLowerCase() === 'status' || activity.propType.toLowerCase() === 'priority' || activity.propType.toLowerCase() === 'timeline'">></span>
-                    <span class="changed-into"
-                        :title="typeof activity.updateTo === 'number' || typeof activity.updateTo === 'string' ? activity.updateTo : ''">{{
-                            activity.updateTo }}</span>
-                </div>
+                <span class="changed-from"
+                    :title="typeof activity.updateFrom === 'number' || typeof activity.updateFrom === 'string' ? activity.updateFrom : ''">{{
+                        activity.updateFrom }}</span>
+                <span
+                    v-if="activity.propType.toLowerCase() === 'status' || activity.propType.toLowerCase() === 'priority' || activity.propType.toLowerCase() === 'timeline'">></span>
+                <span v-else> </span>
+                <span class="changed-into"
+                    :title="typeof activity.updateTo === 'number' || typeof activity.updateTo === 'string' ? activity.updateTo : ''">{{
+                        activity.updateTo }}</span>
             </li>
         </ul>
     </section>
@@ -89,7 +89,7 @@ export default {
     background-color: #fff;
     color: #323338;
     width: 100%;
-    max-height: 3000vh;
+    max-height: 70vh;
     overflow-y: auto;
     font-weight: 300;
     letter-spacing: 0.8px;
@@ -99,27 +99,34 @@ export default {
 .activity-list {
     list-style: none;
     padding: 0;
-    margin: 0;
+    margin-top: 13px;
 }
 
 .activity-item {
     display: grid;
-    grid-template-columns: 0.2fr 0.3fr 1fr 5fr 0.3fr 1.5fr 2fr 2fr;
-    gap: 10px;
-    align-items: center;
-    padding: 10px;
+    grid-template-columns: 1.5fr 1fr 5fr 0.6fr 2fr 3fr 0.2fr 3fr;
+    gap: 4px;
+    padding: 15px;
     border-bottom: 1px solid #e0e0e0;
 
+    .time-passed,
+    .task-name,
+    .activity-type-icon,
+    .activity-type,
+    .changed-from,
+    .changed-into {
+        text-align: left;
+        overflow-x: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+
     .time-passed {
-        width: 40px;
+        width: 1em;
+        letter-spacing: 0.001px;
     }
 
-    .user-image {
-        width: 10px;
-        height: 10px;
-        border-radius: 50%;
-    }
-
+    .user-image,
     .guest {
         width: 10px;
         height: 10px;
@@ -130,28 +137,6 @@ export default {
         align-items: center;
         justify-content: center;
         font-size: 12px;
-    }
-
-    .changed {
-        display: flex;
-        gap: 2px;
-        justify-content: space-between;
-        text-align: center;
-
-        .changed-from,
-        .changed-into {
-            text-align: left;
-            overflow-x: hidden;
-            text-overflow: ellipsis;
-            white-space: nowrap;
-        }
-    }
-
-    .task-name {
-        text-align: left;
-        overflow-x: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
     }
 }
 </style>
