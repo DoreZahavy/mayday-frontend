b3a710ea2cf61e7e2c816963ca04309324539470
Yona <dvsscheme@gmail.com>
2023-08-02 13:42:59 +0300

    homepage styling

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="home-page home-layout">
-    <HomeHeader/>
-    <div  class="welcome">
-
-      <h1>All aspects of work in one complete solution</h1>
+    <HomeHeader />
+    <div class="welcome">
+      <h1>A platform built for a new way of working</h1>
+      <div>
+        <a class="get-started" @click="goToBoard">Get Started<span class="start-arrow" v-html="getSvg('start')"></span></a>
+        <h3>No credit card needed   ✦   Unlimited time on Free plan</h3>
+      </div>
+      <img src="@/assets/img/homepage/static-carousel.avif">
     </div>
   </div>
 </template>
 
 <script>
+import { svgService } from '../services/svg.service'
 import HomeHeader from '@/cmps/HomeHeader.vue'
-export default{
+export default {
   components: {
     HomeHeader
   },
-  created(){
-    document.title="Mayday"
+  methods: {
+    getSvg(iconName) {
+      return svgService.getSvg(iconName)
+    }
+  },
+  created() {
+    document.title = "Mayday"
   }
 }
 </script>
