d036d96ef2342e6c243f30c8a3b5299b3c291449
Din Ben Dor <din1993@gmail.com>
2023-08-05 14:32:19 +0300

    Completed styling for board filters

--- src/cmps/BoardFilter.vue
+++ src/cmps/BoardFilter.vue
@@ -6,9 +6,9 @@
         </span>
         <span v-else style="position: relative; margin-top: -0.1em; margin-right: 1em"><input class="search-input"
                 v-model="searchText" ref="searchInput" placeholder="Search" @blur="editing = false"
-                style="height: 2.25em; width: 20em; border: 0.5px solid royalblue; background-color: transparent; border-radius: 4px; padding-left: 2.27em; font-size: 1em; padding-bottom: 1px;"><span
+                style="height: 2.25em; width: 20em; border: 0.5px solid royalblue; background-color: transparent; border-radius: 4px; padding-left: 2.03em; font-size: 1.15em; margin-bottom: -1px;"><span
                 v-html="getSvg('search')" class="span-common"
-                style="position: absolute; left: 0.6em; top: 4.2px; z-index: -9"></span></span>
+                style="position: absolute; left: 0.6em; top: 6px; z-index: -9"></span></span>
         <el-popover style="width: 800px !important" ref="personPopover" placement="bottom" trigger="click"
             :show-arrow="false" popper-class="person-popover-container">
             <div class="person-filter">
@@ -18,8 +18,8 @@
                         @click="filterByPerson(member._id)"></div>
             </div>
             <template #reference>
-                <span class="span-common" style="margin-top: 0px; gap: 4px;" @click="openPopover('person')">
-                    <span v-html="getSvg('personFilter')" class="span-person"></span>Person
+                <span class="span-common" style="margin-top: -1px; gap: 4px;" @click="openPopover('person')">
+                    <span style="margin-top: 0.5px;" v-html="getSvg('personFilter')" class="span-person"></span>Person
                 </span>
             </template>
         </el-popover>
@@ -82,18 +82,19 @@
             </div>
             <template #reference>
                 <span class="span-common" style="margin-top: 6.4px;" @click="openPopover('filter')">
-                    <span v-html="getSvg('filter')" class="span-filter"></span>Filter
+                    <span style="margin-right: -3.5px; margin-bottom:2px;" v-html="getSvg('filter')"
+                        class="span-filter"></span>Filter
                 </span>
             </template>
         </el-popover>
-        <el-popover ref="sortPopover" placement="bottom" trigger="click" :show-arrow="false">
+        <!-- <el-popover ref="sortPopover" placement="bottom" trigger="click" :show-arrow="false">
             <div></div>
             <template #reference>
                 <span class="span-common" style="margin-top: 0; gap: 0.3em;" @click="openPopover('sort')">
                     <span v-html="getSvg('sortBig')" class="span-sort"></span>Sort
                 </span>
             </template>
-        </el-popover>
+        </el-popover> -->
         <el-popover ref="hidePopover" placement="bottom" trigger="click" :show-arrow="false"
             popper-style="width: 264px; padding: 15px; padding-left: 0px; padding-right: 0px;">
             <div class="hide-filter">
@@ -108,7 +109,8 @@
             </div>
             <template #reference>
                 <span class="span-common" style="margin-top: 0; gap: 0.4em;" @click="openPopover('hide')">
-                    <span v-html="getSvg('hide')" class="span-hide"></span>Hide
+                    <span style="margin-left: -0.5em; margin-top: 0px;" v-html="getSvg('hide')"
+                        class="span-hide"></span>Hide
                 </span>
             </template>
         </el-popover>
