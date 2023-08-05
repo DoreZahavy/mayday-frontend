ced5e715b4cdc88721f7769ec1c6d5828c7841ba
Din Ben Dor <din1993@gmail.com>
2023-08-05 23:04:27 +0300

    Modified the drawer modal functionality and design

--- src/cmps/dynamicCmps/Status.vue
+++ src/cmps/dynamicCmps/Status.vue
@@ -1,10 +1,17 @@
 <template>
   <el-popover ref="popover" placement="bottom" trigger="click" @show="handleTooltipOpen" @hide="handleTooltipClose"
     class="status-popover-container">
+    <!-- <div v-if="!labelEditing"> -->
+    <!-- <div> -->
     <div v-for="label in statusLabelConfig" :key="label.title" :class="label.color" class="status-option"
       @click="onUpdateStatus(label)">
       {{ label.title }}
     </div>
+    <!-- <span @click="labelEditing = true"><span v-html="getSvg('text')"></span>Edit Labels</span> -->
+    <!-- </div> -->
+    <!-- <div v-else>
+      cutsom labels
+    </div> -->
     <template #reference>
       <div v-if="status" class="status status-content fs18" :class="status.color">
         {{ status.title }}
@@ -15,6 +22,7 @@
 </template>
 
 <script>
+import { svgService } from '../../services/svg.service'
 export default {
   name: "Status",
   emits: ['update'],
@@ -25,7 +33,8 @@ export default {
   data() {
     return {
       status: { ...this.info },
-      statusLabelConfig: this.$store.getters.statusLabelConfig
+      statusLabelConfig: this.$store.getters.statusLabelConfig,
+      labelEditing: false
     }
   },
   methods: {
@@ -43,10 +52,14 @@ export default {
       requestAnimationFrame(() => {
         this.$refs.taskFocusedSwitch.blur()
       })
+      this.labelEditing = false
     },
+    getSvg(iconName) {
+      return svgService.getSvg(iconName)
+    }
   },
-  watch:{
-    info(){
+  watch: {
+    info() {
       this.status = { ...this.info }
       // console.log('this.info:', this.info)
     }
