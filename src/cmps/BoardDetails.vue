11493b4c4c33d0dc4c144ba6e8112c581f48537a
Yona <dvsscheme@gmail.com>
2023-08-05 18:21:30 +0300

    group on mobile changed

--- src/cmps/BoardDetails.vue
+++ src/cmps/BoardDetails.vue
@@ -108,10 +108,10 @@ export default {
       try {
         checkedTasksGroups.forEach(group => {
           group.taskIds.forEach(tId => {
-            try{
+            try {
               this.removeTask(group.groupId, tId)
             }
-            catch(err){
+            catch (err) {
               console.log('canny do it')
             }
           })
@@ -145,7 +145,8 @@ export default {
 </script>
 
 <template class="flex">
-  <Container v-if="board && Object.keys(filteredBoard).length === 0" @drop="onDropGrp" class="board-details">
+  <Container v-if="board && Object.keys(filteredBoard).length === 0" @drop="onDropGrp" :drag-begin-delay="500"
+    class="board-details">
 
     <Draggable class="grp-scroll" v-for="(group, idx) in board.groups" :key="group._id">
       <MinimizedGroup :group="group" :groupIdx="idx" @update="updateBoard(group._id, $event)"
